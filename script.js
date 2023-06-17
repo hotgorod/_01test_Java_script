af11d1cde90beea12255ddd27f0bfd524061b40a
hotgorod <hotgorod@gmail.com>
2023-06-17 18:09:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1303,26 +1303,62 @@
 // console.log(calculator.sum());
 
 // ===================================
-const pizzaPalace = {
-  pizzas: ["Ultracheese", "Smoked", "Four meats"],
-  order(pizzaName) {},
-};
-// Change code above this line
+//
+//
+//
+// Необхідно написати логіку обробки замовлення піци.Виконай рефакторинг методу order таким чином,
+//   щоб він приймав другим і третім параметром два колбеки onSuccess і onError.
 
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
-}
+// Якщо у властивості pizzas відсутня піца з назвою з параметра pizzaName,
+//   метод order повинен повертати результат виклику колбека onError, передаючи йому аргументом рядок
+// "There is no pizza with a name <имя пиццы> in the assortment."
+// Якщо у властивості pizzas присутня піца з назвою з параметра pizzaName,
+//   метод order повинен повертати результат виклику колбека onSuccess, передаючи
+//   йому аргументом назву замовленої піци.
+// Після оголошення об'єкта pizzaPalace ми додали колбеки і виклики методів. Будь ласка, нічого там не міняй.
 
-// Callback for onError
-function onOrderError(error) {
-  return `Error! ${error}`;
-}
 
-// Method calls with callbacks
-pizzaPalace.order("Smoked", makePizza, onOrderError);
-pizzaPalace.order("Four meats", makePizza, onOrderError);
-pizzaPalace.order("Big Mike", makePizza, onOrderError);
-pizzaPalace.order("Vienna", makePizza, onOrderError);
 
-console.log(pizzaPalace)
+
+
+
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName, onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       return onSuccess(pizzaName);
+//     }
+//     else return onError(`There is no pizza with a name ${pizzaName} in the assortment.`)
+//   },
+// };
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `There is no pizza with a name ${error} in the assortment.`;
+// }
+
+// // Method calls with callbacks
+// console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
+
+// =======================================================
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Change code below this line
+
+//   orderedItems.forEach(function (item, i, orderedItems) {
+//    totalPrice += item
+//  })
+
+//   // Change code above this line
+//   return totalPrice;
+// }
